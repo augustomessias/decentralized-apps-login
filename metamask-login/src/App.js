24d11015eec18da649f197f656378097eb136ef4
mineironovale <augustomessias@pm.me>
2022-08-29 16:41:08 -0300

    Create login interface with functions for SignIn and SignUp

--- metamask-login/src/App.js
+++ metamask-login/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function doSignIn() {
+    alert("Sign In");
+  }
+
+  function doSignUp() {
+    alert("Sign Up");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+        <div>
+          <button onClick={doSignIn}>Sign In with MetaMask</button>
+          <button onClick={doSignUp}>Sign Up with MetaMask</button>
+        </div>
       </header>
     </div>
   );
