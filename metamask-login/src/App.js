38d3b2f851d36dc7324823068344a183ead9a5e6
mineironovale <augustomessias@pm.me>
2022-08-29 17:36:04 -0300

    Configure functions doSignIn for authentication through metamask browser extension; and doSignUp to redirect the user to the metamask download page

--- metamask-login/src/App.js
+++ metamask-login/src/App.js
@@ -1,22 +1,41 @@
 import './App.css';
+import { useState } from 'react';
+import { ethers } from "ethers";
 
 function App() {
 
+  const [error, setError] = useState("");
+  const [wallet, setWallet] = useState("");
+
   function doSignIn() {
-    alert("Sign In");
+    if(!window.ethereum) return setError("No MetaMask wallet found");
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    provider.send("eth_requestAccounts", [])
+      .then(accounts => {
+        if(!accounts || !accounts.length) {
+          return setError("No crypto wallet found");
+        }
+
+        localStorage.setItem("wallet", accounts[0]);
+        setWallet(accounts);
+      })
+      .catch(err => setError(err.message))
   }
 
   function doSignUp() {
-    alert("Sign Up");
+    window.location.href = "https://metamask.io/download/";
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Login</h1>
+        <div>{error}</div>
+        <div>{wallet}</div>
         <div>
           <button onClick={doSignIn}>Sign In with MetaMask</button>
-          <button onClick={doSignUp}>Sign Up with MetaMask</button>
+          <button onClick={doSignUp}>Sign Up on MetaMask.io</button>
         </div>
       </header>
     </div>
